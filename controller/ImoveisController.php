dd5247f6671356f518873c6e9c443a9ba04a6185
Gabriel Garcia <garcia.gabrielrodrigues1@gmail.com>
2024-06-24 01:49:38 -0300

    Add pagina publica de imoveis

--- controller/ImoveisController.php
+++ controller/ImoveisController.php
@@ -17,7 +17,8 @@ class ImoveisController
         //Se não estiver logado
         if (!SessionService::isLogued()) {
             //Versão publica
-            echo "Fazer versão publica imoveis";
+            //echo "Fazer versão publica imoveis";
+            $this->publicListImoveis();
             return;
         }
 
@@ -30,13 +31,13 @@ class ImoveisController
 
         /*/Validar campos preenchidos
         $args = func_get_args();
-        $valid = false;
+        $empty = false;
         foreach ($args as $k => $v) {
             if ($k == 0) {
-                $valid = ($v == "");
+                $empty = ($v == "");
                 continue;
             }
-            $valid = $valid && ($v == "");
+            $empty = $empty && ($v == "");
         }*/
 
         $filters = [
@@ -60,23 +61,77 @@ class ImoveisController
             "active" => ""
         ];
 
-        $valid = false;
+        $empty = false;
         foreach ($filters as $k => $v) {
             if ($k == "id") {
-                $valid = ($v == "");
+                $empty = ($v == "");
                 continue;
             }
-            $valid = $valid && ($v == "");
+            $empty = $empty && ($v == "");
         }
 
         //Se todos os campos forem vazios
-        if ($valid) {
+        if ($empty) {
             $api = new CUrlRequest(Api::URLBASE . "imovel/listar/all", SessionService::getToken());
-            $result = $api([]);
+            $result = $api();
         } 
         else {
             $api = new CUrlRequest(Api::URLBASE . "imovel/listar", SessionService::getToken());
             $result = $api($filters);
+        }
+        
+        //Validar saida
+        if($api->getHttpCode() == 401){
+            //Problemas na sessão
+            SessionService::sessionLogout();
+            header("Location: " . RouteController::RootRoute() . "/login/" . ViewResponseCodes::SESSION_EXPIRED);
+            exit();
+        }
+        
+        $view = new ImoveisView();
+        $view->listImoveis($result['retorno']);
+    }
+
+    private function publicListImoveis(){
+        
+        $api =  null;
+        $result = [];
+
+        $filters = [
+            "tipo_imovel" => $_POST['tipo_imovel'] ?? "",
+            "endereco" => $_POST['endereco'] ?? "",
+            "cidade" => $_POST['cidade'] ?? "",
+            "estado" => $_POST['estado'] ?? "",
+            "CEP" => $_POST['CEP'] ?? "",
+            "valor_aluguel" => $_POST['valor_aluguel'] ?? "",
+            "max_valor_aluguel" => $_POST['max_valor_aluguel'] ?? "",
+            "area" => $_POST['area'] ?? "",
+            "max_area" => $_POST['max_area'] ?? "",
+            "quartos" => $_POST['quartos'] ?? "",
+            "max_quartos" => $_POST['max_quartos'] ?? "",
+            "banheiros" => $_POST['banheiros'] ?? "",
+            "max_banheiros" => $_POST['max_banheiros'] ?? "",
+            "vagas_garagem" => $_POST['vagas_garagem'] ?? "",
+            "max_vagas_garagem" => $_POST['max_vagas_garagem'] ?? ""
+        ];
+
+        $empty = false;
+        foreach ($filters as $k => $v) {
+            if ($k == "tipo_imovel") {
+                $empty = ($v == "");
+                continue;
+            }
+            $empty = $empty && ($v == "");
+        }
+
+        //Se todos os campos forem vazios
+        if ($empty) {
+            $api = new CUrlRequest(Api::URLBASE . "public/imovel/listar/all");
+            $result = $api([], CUrlRequest::GET);
+        } 
+        else {
+            $api = new CUrlRequest(Api::URLBASE . "public/imovel/listar");
+            $result = $api($filters, CUrlRequest::GET);
         }
         
         //Validar saida
